26093a1b1193410be5ead6da49fe0164cd12703c
Christoph Fabianek <christoph.fabianek@gmail.com>
2022-02-04 10:58:20 +0100

    npm test checks update

--- cli/docker/test/run_npm.sh
+++ cli/docker/test/run_npm.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 docker run -it node:17 bash -c "npm i --silent -g soya-cli@latest &>/dev/null; soya pull Person_Test" > tmp.doc
 if ! cmp -s tmp.doc checks/person_test.jsonld ; then
-	echo "error: npm soya install failed"
+	echo "error: npm install soya-cli failed"
 	rm tmp*.doc
 	exit 1
 else
-	echo "passed: npm install"
+	echo "passed: npm install soya-cli"
 fi
 rm tmp*.doc
