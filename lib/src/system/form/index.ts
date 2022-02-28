82bfbc85b5a02e382c5934bf1d8f9ca414f67844
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2022-02-28 13:54:20 +0100

    Improved form rendering

--- lib/src/system/form/index.ts
+++ lib/src/system/form/index.ts
@@ -1,97 +1,176 @@
-import rdf from "rdf-ext";
-const namedNode = rdf.namedNode;
-
+import { FormRenderError } from "../../errors";
 import { SoyaDocument } from "../../interfaces";
 import { parseJsonLd } from "../../utils/rdf";
+import { SparqlQueryBuilder } from "../../utils/sparql";
 import { JsonSchema, Layout, SoyaForm } from "./interfaces";
 
 export * from './interfaces';
 
-export const getSoyaForm = async (soyaStructure: SoyaDocument): Promise<SoyaForm> => {
-  // search for base
-  // at first we only support one base
-  const dataSet = await parseJsonLd(soyaStructure);
-  // @ts-expect-error
-  const mainClass = dataSet.match(
-    undefined,
-    namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-    namedNode('https://www.w3.org/2002/07/owl#Class'),
-  ).toArray()[0];
+const getLastUriPart = (uri: string): string | undefined => {
+  const split = uri.split('/');
+  return split[split.length - 1];
+}
 
-
-  // all properties
-  const allProps = dataSet.match(
-    undefined,
-    namedNode(`http://www.w3.org/1999/02/22-rdf-syntax-ns#type`),
-    namedNode('https://www.w3.org/2002/07/owl#DatatypeProperty'),
-  ).toArray();
-
+const handleClass = async (
+  name: string,
+  builder: SparqlQueryBuilder,
+  classUri: string,
+  rootLayout: Layout,
+  layoutSubPath: string = '',
+): Promise<JsonSchema> => {
   const schema: JsonSchema = {
     type: 'object',
     properties: {},
-  };
-  const ui: Layout = {
-    type: 'VerticalLayout',
-    elements: [],
-  };
-  const retVal: SoyaForm = {
-    schema,
-    ui,
   };
 
   // this if is just here to please typescript
   // obviously it does not get that schema.properties is always set
   // weirdly it thinks it could undefined...
   if (!schema.properties)
-    return retVal;
+    return schema;
+
+  const layout: Layout = {
+    type: 'Group',
+    // @ts-expect-error FIXME: label is not recognized, probably an error in official types
+    // capitalize first letter
+    label: name.charAt(0).toUpperCase() + name.slice(1),
+    elements: [],
+  }
+  rootLayout.elements.push(layout);
+
+  const allProps = await builder.query(`
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?prop ?range WHERE {
+      ?prop rdfs:domain <${classUri}> .
+      ?prop a owl:DatatypeProperty .
+      ?prop rdfs:range ?range .
+    }`);
 
   for (const prop of allProps) {
-    const dt = dataSet.match(
-      prop.subject,
-      namedNode(`https://www.w3.org/2000/01/rdf-schema#range`),
-      undefined,
-    ).toArray()[0];
+    const propUri = prop.get('?prop');
+    const range = prop.get('?range');
 
-    if (!dt)
+    if (!range || !propUri)
       continue;
 
-    // @ts-ignore
-    let URL = globalThis.URL;
-    if (!URL) // node envs
-      URL = import('url');
-
-    const dtUrl = new URL(dt.object.value);
-    if (dtUrl.pathname !== '/2001/XMLSchema')
-      continue;
-
-    let type: string | undefined;
-    let format: string | undefined;
-
-    switch (dtUrl.hash.substring(1).toLowerCase()) {
-      case 'date':
-        type = 'string';
-        format = 'date';
-        break;
-      default:
-        type = 'string';
-    }
-
-    const dtSubParts = dt.subject.value.split('/');
-    const propName = dtSubParts[dtSubParts.length - 1];
+    const propName = getLastUriPart(propUri);
 
     if (!propName)
       continue;
 
-    schema.properties[propName] = {
-      type,
-      format,
+    // check if URI exists as a graph object
+    const refRange = await builder.query(`
+      SELECT * WHERE {
+        <${range}> ?p ?o .
+      }`);
+
+    if (refRange.length !== 0) {
+      // it's a graph object
+      // start recursion
+      schema.properties[propName] = await handleClass(
+        propName,
+        builder,
+        range,
+        rootLayout,
+        `${layoutSubPath}${propName}/properties/`
+      );
+    } else {
+      // it's something else (e.g. XML schema)
+      let type: string | undefined;
+      let format: string | undefined;
+
+      // try to get hash from XML schema string
+      switch (range.split('#')[1]) {
+        case 'date':
+          type = 'string';
+          format = 'date';
+          break;
+        default:
+          type = 'string';
+      }
+
+      // see if we can get some information out of a SHACL shape
+      const shaclConstraintList = await builder.query(`
+        PREFIX sh: <http://www.w3.org/ns/shacl#>
+        SELECT ?class ?prop ?min ?max WHERE {
+          ?class a sh:NodeShape .
+          ?class sh:property ?prop .
+          ?class sh:targetClass <${classUri}> .
+          ?prop sh:minCount ?min .
+          ?prop sh:maxCount ?max .
+        }`);
+
+      let minLength: string | undefined;
+      let maxLength: string | undefined;
+
+      if (shaclConstraintList[0]) {
+        const shaclConstraint = shaclConstraintList[0];
+
+        minLength = shaclConstraint.get('?min') || undefined;
+        maxLength = shaclConstraint.get('?max') || undefined;
+      }
+
+      schema.properties[propName] = {
+        type,
+        format,
+
+        minLength: minLength ? parseInt(minLength) : undefined,
+        maxLength: maxLength ? parseInt(maxLength) : undefined,
+      }
+      layout.elements.push({
+        type: 'Control',
+        // @ts-expect-error FIXME: scope is not recognized, probably an error in official types
+        scope: `#/properties/${layoutSubPath}${propName}`,
+      });
     }
   }
 
-  ui.elements = Object.keys(schema.properties).map(propName => ({
-    type: 'Control',
-    scope: `#/properties/${propName}`,
-  }));
+  return schema;
+}
+
+export const getSoyaForm = async (soyaStructure: SoyaDocument): Promise<SoyaForm> => {
+  const dataSet = await parseJsonLd(soyaStructure);
+  const builder = new SparqlQueryBuilder(dataSet);
+
+  const classes = await builder.query(`
+  PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  SELECT ?s WHERE {
+    ?s a owl:Class .
+    # exclude all classes that are nested in a parent class
+    FILTER NOT EXISTS {
+      ?rel rdfs:range ?s .
+    }
+  }`);
+
+  if (!classes[0])
+    throw new FormRenderError('Main class not found.');
+
+  // we only support one base for now
+  const mainClass = classes[0];
+  const mainClassUri = mainClass.get('?s');
+
+  if (!mainClassUri)
+    throw new FormRenderError('Main class URI not found.');
+
+  const name = getLastUriPart(mainClassUri);
+  if (!name)
+    throw new FormRenderError('Main class name not found.');
+
+  const ui: Layout = {
+    type: 'VerticalLayout',
+    elements: [],
+  };
+  const retVal: SoyaForm = {
+    schema: await handleClass(
+      name,
+      builder,
+      mainClassUri,
+      ui
+    ),
+    ui,
+  };
 
   return retVal;
 }
