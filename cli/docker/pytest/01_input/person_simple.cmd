5a7f156aaab24725df4e0d52129ff2d563b701b9
Christoph Fabianek <christoph.fabianek@gmail.com>
2022-03-08 00:56:52 +0100

    general tests

---
+++ cli/docker/pytest/01_input/person_simple.cmd
@@ -0,0 +1,1 @@
+soya init
--- cli/docker/pytest/test_general.py
+++ cli/docker/pytest/test_general.py
@@ -14,20 +14,14 @@ def test_repo():
 
 # doc: https://pypi.org/project/pytest-subprocess/
 cwd = os.getcwd()
-@pytest.mark.parametrize('input',  glob.glob(cwd+'/01_input/*'))
+@pytest.mark.parametrize('input',  glob.glob(cwd+'/01_input/*.doc'))
 def test_01_simple(fp, input):
     fp.allow_unregistered(True)
-    with open(input) as f:
-        content = f.read()
+    with open(input.replace(".doc", ".cmd")) as f:
+        command = f.read()
     with open(input.replace("_input/", "_output/")) as f:
         result = f.read()
-
-    process = subprocess.Popen(
-            ["soya", "init"], 
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-            universal_newlines=True,
-        )
-    out, _ = process.communicate(input=content)
-
+    command = "cat " + input + " | " + command
+    process = subprocess.run(command, shell=True, capture_output=True, text=True)
     assert process.returncode == 0
-    assert out == result
+    assert process.stdout.strip() == result.strip()
