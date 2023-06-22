b6f85747cabd5c3293260e849ead7894155a0c7b
Christoph Fabianek <christoph.fabianek@gmail.com>
2023-06-22 09:46:36 +0200

    fix and extend pytests

---
+++ cli/docker/pytest/02_input/senml.cmd
@@ -0,0 +1,1 @@
+soya transform SenML_Transformation
---
+++ cli/docker/pytest/test_usecases.py
@@ -0,0 +1,32 @@
+import pytest
+import os
+import sys
+import glob
+import requests
+import subprocess
+from pathlib import Path
+import re
+
+repo = "https://soya.ownyourdata.eu"
+
+
+# doc: https://pypi.org/project/pytest-subprocess/
+cwd = os.getcwd()
+@pytest.mark.parametrize('input',  glob.glob(cwd+'/02_input/*.doc'))
+def test_01_simple(fp, input):    
+    fp.allow_unregistered(True)
+    with open(input) as f:
+        content = f.read()
+    with open(input.replace(".doc", ".cmd")) as f:
+        command = f.read()
+    with open(input.replace("_input/", "_output/")) as f:
+        result = f.read()
+    if len(content) > 0:
+        # correctly escape spaces in filenames
+        # this is necessary if filename is used in command directly
+        _input = re.sub(r"\s", "\\ ", input)
+        command = "cat " + _input + " | " + command
+    process = subprocess.run(command, shell=True, capture_output=True, text=True)
+    assert process.returncode == 0
+    if len(result) > 0:
+        assert process.stdout.strip() == result.strip()
