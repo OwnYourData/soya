d265c759162c34ad43f0c01b404a849b9d12527f
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2023-08-23 09:21:28 +0200

    Fixed list container type

--- lib/src/system/yaml2soya/index.ts
+++ lib/src/system/yaml2soya/index.ts
@@ -28,7 +28,22 @@ const handleBase = (doc: IntSoyaDocument, base: any) => {
       specifiedDataType = genericMatches[2];
     }
 
-    const {dataType, isXsd} = tryUseXsdDataType(specifiedDataType);
+    let { dataType, isXsd } = tryUseXsdDataType(specifiedDataType);
+
+    // currently we don't care about the specific container type
+    // and treat everything like a list
+    // here we also see that the contained data type is basically
+    // lost, just rdf:List remains
+    // this is due to the limitation of rdf not knowing about the 
+    // datatype that's used inside the list. rdf just does not care
+    // This COULD be properly solved by adding some SHACL statements
+    // to restrict what data types are allowed to be used inside the list
+
+    // conclusion: this is a current limitation of SOyA in a way
+    // that information about the contained data type is just lost
+    // at this point (because of the limitation of rdf)
+    if (containerType)
+      dataType = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#List';
 
     const graphItem: any = {
       '@id': attrName,
@@ -39,9 +54,13 @@ const handleBase = (doc: IntSoyaDocument, base: any) => {
       'range': dataType,
     };
 
-    if (containerType)
-      // lowercase, because most container types in json-ld are lowercase
-      graphItem['soya:container_type'] = containerType.toLowerCase();
+    // This is some legacy code where we introduced a proprietary property
+    // which we anyway didn't use. However this could be interesting
+    // if in the furture we want to bring container types to life again.
+
+    // if (containerType)
+    //   // lowercase, because most container types in json-ld are lowercase
+    //   graphItem['soya:container_type'] = containerType.toLowerCase();
 
     graph.push(graphItem);
   }
